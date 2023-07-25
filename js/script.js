5324286a63b75a6469dbf0193eb16c301387a9c2
sebastianramirez <sebastian_ramirezro@soy.sena.edu.co>
2023-07-24 22:07:52 -0500

    comenzamos con javascript

---
+++ js/script.js
@@ -0,0 +1,31 @@
+console.log("analisis y desarrollo de software")
+/* este tipo de comentario es para trabajar en 
+difrente linea de codigo */ 
+// es para manejaren una sola linea
+
+//var edad=25;
+
+//console.log("Mi edad es: "+edad) // el simbolo "+" va ha funcionar para concatenar 
+//alert("Mi edad es: "+edad )
+//document.write("Mi edad es: "+edad )
+//const nom="sebastian";
+//let num=2.5;
+//booleano=false;
+/*
+const nom="sebastian"
+const ape="ramirez"
+alert("mi nobre es "+nom+" y mi apellido es "+ape)*/
+/* operadores arimeticos 
+const num1=5
+const num2=3
+const div = num1/num2
+alert("la suma es "+div)*/
+//console.log(typeof booleano); es para definir el tipo de dato 
+
+let nom=prompt("escriba el nombre")
+let edad=prompt("escriba su edad")
+let peso=prompt("escriba su peso")
+let ciud=prompt("escriba su ciudad")
+let direc=prompt("escriba su direccion")
+alert("mi nombre es "+nom+", mi edad es "+edad+"años"+", yo peso "+peso+"kg"+ ", vivo en "+ciud+", mi direccion es "+direc)
+/* mostrar en una alerta nombres y apellidos completos, edad, peso y ciudad de residencia y direcion*/
