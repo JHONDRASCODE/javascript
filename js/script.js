944948138ac5889bdf22c2a08574ab8a620b52c1
JHONDRASCODE <132631552+JHONDRASCODE@users.noreply.github.com>
2023-07-28 22:32:19 -0500

    todos los ejercicios

--- js/script.js
+++ js/script.js
@@ -29,3 +29,59 @@ let ciud=prompt("escriba su ciudad")
 let direc=prompt("escriba su direccion")
 alert("mi nombre es "+nom+", mi edad es "+edad+"años"+", yo peso "+peso+"kg"+ ", vivo en "+ciud+", mi direccion es "+direc)
 /* mostrar en una alerta nombres y apellidos completos, edad, peso y ciudad de residencia y direcion wjcvwegcvjw*/
+
+/*Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra. */
+/*let compr = prompt ("introduzca el precio de su compra");
+let descuento = 25/100
+let compr1 = parseInt(compr*descuento)
+alert ("el valor total que debera pagar por su compra es de "+compr1) */
+
+/*Suponga que un individuo desea invertir su capital en un banco y desea saber
+cuánto dinero ganara después de un mes si el banco paga a razón de 2%
+mensual.*/
+/*let inv = prompt ("cuanto dinero piensa en invertir")
+let pgan = 2/100
+let gans= (inv * pgan);
+let din = parseInt(inv) + parseInt(pgan);
+alert("dinero invertido")*/
+
+/*Un alumno desea saber cuál será su calificación final en la materia de
+Algoritmos. Dicha calificación se compone de los siguientes porcentajes:
+a. 55% del promedio de sus tres calificaciones parciales.
+b. 30% de la calificación del examen final.
+c. 15% de la calificación de un trabajo final.*/
+
+/*let cali1 = prompt("ingrsar nota de su primera calificacion");
+let cali2 = prompt("ingrsar nota de su segunda calificacion");
+let cali3 = prompt("ingrsar nota de su tercera calificacion");
+let exfnl = prompt("ingrese la nota de su examen final ");
+let tfnl = prompt("ingrese la nota de su trabajp final")
+let cal = parseInt(cali1+cali2+cali3)/3
+let porcali = 55/100
+let porexf = 30/100
+let portf = 15/100
+let porcf =*/
+
+/*El dueño de una tienda compra un artículo a un precio determinado. Obtener el
+precio en que lo debe vender para obtener una ganancia del 30%.*/
+
+/*let partlo = pront ("porfavor ingrese el precio del articulo");
+ let parto = parseInt (partlo);
+ let pventa = partlo + (parto * 0.30);
+ console.log =("el precio de venta para obtener el 30% es: "+ pventa);*/
+
+ /*Un taxi cobra por una carrera 15 mil pesos por kilómetro recorrido y 2 mil por
+ minuto. Determine el monto a pagar por una carrera.*/
+
+/*let km = prompt("cuantos kilometros ha recorrido? ");
+let min = prompt("cuantos minutos demoro en recorrer esta distancia? ");
+let dkm = 15000;
+let dmin = 2000;
+let dtkm = parseInt(dkm) * parseInt(km);
+let dtmin = parseInt(dmin) * parseInt(min);
+let vcrr = parseInt(dtkm) * parseInt(dtmin);
+alert("El valor de esta carrera es de: "+vcrr);*/
+
+
+
