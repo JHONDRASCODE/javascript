8ea17946be64b8bc6047f2094bf490be4bf76769
JHONDRASCODE <132631552+JHONDRASCODE@users.noreply.github.com>
2023-08-01 16:21:08 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,5 +83,74 @@ let dtmin = parseInt(dmin) * parseInt(min);
 let vcrr = parseInt(dtkm) * parseInt(dtmin);
 alert("El valor de esta carrera es de: "+vcrr);*/
 
+/*Ejercicio 6 - Realizara un programa en el cual se van a leer tres números y determinar el
+cuadrado de cada uno de ellos. */
+
+let num1 = prompt("ingrese el primer número")
+let num2 = prompt("ingrese el segundo número")
+let num3 = prompt("ingrese el tercer núemro")
+let cua1 = Math.pow(num1, 2);
+let cua2 = Math.pow(num2, 2);
+let cua3 = Math.pow(num3, 2);
+
+alert("el cuadrado de los tres numeros es de " +cua1+ " el primer numero," +cua2+" el segundo numero y " +cua3+ " el tercer numero");
+
+/*Ejercicio 7 - En un hospital existen tres áreas: Ginecología, Pediatría, Traumatología. El
+presupuesto anual del hospital se reparte conforme a la sig. tabla:
+Area          : Porcentaje del presupuesto
+ginecología   : 40%
+traumatología : 30% 
+pediatría     : 30%
+Obtener la cantidad de dinero que recibirá cada área, para cualquier monto
+presupuestal.*/
+
+let presupuesto = prompt("Ingrece el presepuesto de este año en el hospital");
+let gine = 40/100;
+let trau = 30/100;
+let pedi = 30/100;
+let resg = parseInt(presupuesto*gine);
+let rest = parseInt(presupuesto*trau);
+let resp = parseInt(presupuesto*pedi);
+
+alert("el presupuesto del hopital queda dividido de la siguiente manera: $" +resg+ " en el area de ginecología,$" +rest+ " en el area de traumatología y $" +resp+ " en el area de pediatría.");
+
+/*Ejercico 8 - El dueño de una tienda compra un artículo a un precio determinado. Obtener el
+precio en que lo debe vender para obtener una ganancia del 30%. "REPETIDO"*/
+
+/*Ejercicio 9 - Tres personas deciden invertir su dinero para fundar una empresa. Cada una de
+ellas invierte una cantidad distinta. Obtener el porcentaje que cada quien invierte
+con respecto a la cantidad total invertida. */
+
+let pers1 = prompt("Ingrese la cantidad de dinero invertido por la primera persona");
+let pers2 = prompt("Ingrese la cantidad de dinero invertido por la segunda persona");
+let pers3 = prompt("Ingrese la cantidad de dinero invertido por la tercera persona");
+let totalp = parseInt(pers1)+ parseInt(pers2)+ parseInt(pers3);
+let tper1 = parseInt(pers1)* parseInt(100/totalp);
+let tper2 = parseInt(pers2)* parseInt(100/totalp);
+let tper3 = parseInt(pers3)* parseInt(100/totalp);
+
+alert("El porcentaje que cada persona invirtio en la fundacion de la empresa es de " +tper1+ "% la primera persona, " +tper2+ "% la segunda persona y " +tper3+ "% la tercera persona.");
+
+/*Ejercicio 10 - Realizar una calculadora con las operaciones básicas suma, división,
+multiplicación y división. */
+
+let opcion = prompt("1. suma 2. resta 3. division 4. multiplcacion");
+let nume1 = prompt("digite el numero 1");
+let nume2 = prompt("digite el numero 2");
+
+if (opcion==1){
+    suma=parseInt(nume1)+parseInt(nume2);
+    alert(suma);
+} else if(opcion==2){
+    resta=parseInt(nume1)-parseInt(nume2);
+    alert(resta);
+} else if(opcion==3){
+    division=parseInt(nume1)/parseInt(nume2);
+    alert(division);
+} else {
+    multiplicacion=parseInt(nume1)*parseInt(nume2);
+    alert(multiplicacion);
+}
+
 
 
